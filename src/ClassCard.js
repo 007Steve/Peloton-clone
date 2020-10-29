e22847f0c57de39f354d7d7cc5b1ad5f453d0286
007steve <stephen.plummer1.sp@gmail.com>
2020-10-29 12:57:33 -0400

    added link to class card

--- src/ClassCard.js
+++ src/ClassCard.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './ClassCard.css'
-function ClassCard({title}) {
+import { Link} from 'react-router-dom'
+function ClassCard({title,id}) {
     return (
         <div className="classCard__container">
             <div className="classCard__card">
+            <Link to={`/workout/${id}`}>
                 <img id="image" src="" alt=""/>
                 <h2 id="title__text">{title}</h2>
+                </Link>
             </div>
         </div>
     )
