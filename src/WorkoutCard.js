3698cefa68c23d823bd8fde3fbe552c38fcdae7c
007steve <stephen.plummer1.sp@gmail.com>
2020-10-29 12:55:34 -0400

    added use params and class data to  workout page

--- src/WorkoutCard.js
+++ src/WorkoutCard.js
@@ -4,7 +4,7 @@ function WorkoutCard({time,title,coach,type, mins}) {
     return (
         <div className="WorkoutCard__conatainer">
             <div className="WorkoutCard__card">
-                <img className="WorkoutCard__image" src=" " alt=""/>
+               
                 <p id="time">{time}</p>
                 <div className="WorkoutCard__title">
                     <p className="WorkoutCard__title-texts">{title}</p>
