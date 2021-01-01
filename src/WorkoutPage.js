bddf9fcd042b23511d29544573332fb2491d1c4f
007steve <stephen.plummer1.sp@gmail.com>
2021-01-01 15:04:05 -0500

    fixed iframe

--- src/WorkoutPage.js
+++ src/WorkoutPage.js
@@ -2,30 +2,27 @@ import React, { useEffect, useState } from "react";
 import "./WorkoutPage.css";
 import Icon from "./Icon.js";
 import Nav from "./Nav.js";
-import VideoCard from "./VideoCard";
 import { useParams } from "react-router-dom";
 import ClassData from "./classData.js";
-import TuneIcon from '@material-ui/icons/Tune';
-
+import TuneIcon from "@material-ui/icons/Tune";
 
 function WorkoutPage() {
   const { id } = useParams();
   const classDataInfo = ClassData.find((data) => data.id === id);
-  const query = "workouts"
+  const query = "workouts";
   const [videos, setVideos] = useState([]);
   const apiKey = "AIzaSyDtJjWW6ktLwEq2yr5bEtWz5WaVTwcDHmQ";
   const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${apiKey}&type=video&q=${classDataInfo.title}${query}&maxResults=12`;
- 
+
   useEffect(() => {
     async function fetchVideos() {
       fetch(url)
-      .then( response => response.json())
-      .then(dataVideo => setVideos(dataVideo.items))
-      ;
+        .then((response) => response.json())
+        .then((dataVideo) => setVideos(dataVideo.items));
     }
     fetchVideos();
   }, [url]);
-  
+
   return (
     <div className="workoutpage">
       <Nav />
@@ -66,18 +63,32 @@ function WorkoutPage() {
       </div>
       <div className="workoutpage__videos">
         <div className="workoutpage__info">
-            <p>{classDataInfo.title}</p>
-            <div  className="workoutpage__icon">
-                <TuneIcon/>
-                <p>Filter</p>
-            </div>
+          <p>{classDataInfo.title}</p>
+          <div className="workoutpage__icon">
+            <TuneIcon />
+            <p>Filter</p>
+          </div>
         </div>
-            
-            <div className="workoutpage__videosContainer">
-          {videos.map(video => ( <VideoCard video={video.id.videoId} key={video.id.videoid}  id={video.Id}/>))
-            }
-            </div>
+
+        <div className="workoutpage__videosContainer">
+          {videos.map((video) => (
+            <iframe
+            title="youtube"
+              width="450"
+              height="300"
+              src={`https://www.youtube.com/embed/${video.id.videoId}`}
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+             
+              allowfullscreen="allowfullscreen"
+              mozallowfullscreen="mozallowfullscreen"
+              msallowfullscreen="msallowfullscreen"
+              oallowfullscreen="oallowfullscreen"
+              webkitallowfullscreen="webkitallowfullscreen"
+            ></iframe>
+          ))}
         </div>
+      </div>
     </div>
   );
 }
