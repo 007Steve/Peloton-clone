82e118bfa96fd8844269649abe4b49be6ef237d3
007steve <stephen.plummer1.sp@gmail.com>
2021-01-01 15:04:36 -0500

    fixed img alt

--- src/Login.js
+++ src/Login.js
@@ -3,8 +3,19 @@ import './Login.css'
 function Login() {
     return (
         <div className="login_container">
-            <img src="https://www.ldg.com/wp-content/uploads/2018/04/PelotonLogo.png"/>
+            <img src="https://www.ldg.com/wp-content/uploads/2018/04/PelotonLogo.png" alt="logo"/>
             <a id="login__btn" href="/">Login</a>
+            {/* <form>
+                <label htmlFor="uesrname">Username</label>
+                <br/>
+               <input type="text" placeholder="username"/>
+               <br/>
+               <label htmlFor="password">Password</label>
+               <br/>
+               <input type="text" placeholder="username"/>
+               <br/>
+               <input type="submit" value="submit"/>
+            </form> */}
         </div>
     )
 }
